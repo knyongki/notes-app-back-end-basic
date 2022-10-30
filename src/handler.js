cf52e4dfb66024fca1315510b474069fbdcc1a75
knyongki <yyongkisetiawan@gmail.com>
2022-10-30 22:51:05 +0700

    membuat project notes app

---
+++ src/handler.js
@@ -0,0 +1,43 @@
+const { nanoid } = require('nanoid');
+const notes = require('./notes');
+
+const addNoteHanler = (request, h) => {
+  const { title, tags, body } = request.payload;
+
+  const id = nanoid(16);
+  const createAt = new Date().toISOString();
+  const updateAt = createAt;
+
+  const newNote = {
+    title,
+    tags,
+    body,
+    id,
+    createAt,
+    updateAt,
+  };
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteID: id,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+};
+
+module.exports = { addNoteHanler };
