501a3749851a7891634030627e7f5d35447027b2
knyongki <yyongkisetiawan@gmail.com>
2022-11-01 20:24:55 +0700

    menambahkan edit note

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler } = require('./handler');
+const {
+  addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+} = require('./handler');
 
 const routes = [{
   method: 'POST',
@@ -14,6 +16,11 @@ const routes = [{
   method: 'GET',
   path: '/notes/{id}',
   handler: getNoteByIdHandler,
+},
+{
+  method: 'PUT',
+  path: '/notes/{id}',
+  handler: editNoteByIdHandler,
 },
 ];
 
